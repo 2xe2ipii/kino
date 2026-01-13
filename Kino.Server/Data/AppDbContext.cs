28ba225d5b0fac4aabcbc95997c2fb48e7767b7b
Drexler Reyes <2xe2ipi@gmail.com>
2026-01-13 17:20:37 +0800

    Backend configured: Postgres, EF Core, and Swagger setup

---
+++ Kino.Server/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Kino.Server.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
