d93581f8f4a4f8886b0b650a25a2aca4da56f4f9
Drexler Reyes <2xe2ipi@gmail.com>
2026-04-26 01:30:22 +0800

    feat: add GoogleOAuthProvider, UsernameModal to app root; fix public profile route

--- kino-client/src/App.tsx
+++ kino-client/src/App.tsx
@@ -1,18 +1,20 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthModal } from './components/modals/AuthModal';
+import { UsernameModal } from './components/modals/UsernameModal';
 import Home from './pages/Home';
-import Profile from './pages/Profile'; 
-import PublicProfile from './pages/PublicProfile'; // <--- Import
+import Profile from './pages/Profile';
+import PublicProfile from './pages/PublicProfile';
 import Diary from './pages/Diary';
 
 function App() {
   return (
     <Router>
         <AuthModal />
+        <UsernameModal />
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/profile" element={<Profile />} /> 
-            <Route path="/member/:userId" element={<PublicProfile />} /> {/* <--- New Route */}
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/u/:username" element={<PublicProfile />} />
             <Route path="/diary" element={<Diary />} />
             <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
--- kino-client/src/main.tsx
+++ kino-client/src/main.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { AuthProvider } from './context/AuthContext'; // <--- This was missing
+import { AuthProvider } from './context/AuthContext';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthProvider>  {/* <--- Wraps the App so context works */}
-      <App />
-    </AuthProvider>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </GoogleOAuthProvider>
   </React.StrictMode>,
 );
